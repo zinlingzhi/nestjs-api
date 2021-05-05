af35a07f47ac152bf909a1d3593c2e69f41e7985
decaf-programmer <decaf.programmer@gmail.com>
2021-05-05 14:48:59 +0800

    Update app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PollsModule } from './polls/polls.module';
+import { PollAnswersModule } from './poll_answers/poll_answers.module';
+import { TenantsModule } from './tenants/tenants.module';
 
 @Module({
-  imports: [],
+  imports: [PollsModule, PollAnswersModule, TenantsModule],
   controllers: [AppController],
   providers: [AppService],
 })
